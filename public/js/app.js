5158682e9953b7cdaa0ee50500893d7574af7563
unknown <drew_hoffer@hotmail.com>
2020-03-12 15:11:19 -0400

    Updated create.html

--- public/js/app.js
+++ public/js/app.js
@@ -7,8 +7,10 @@ const $signInForm = document.querySelector('#sign-in-form');
 
 
 $signInForm.addEventListener('submit', async (e) =>{
-	error.innerHTML = '';
+	document.getElementById("error").style.display = "none";
+	error.innerHTML = '<ul>';
 	e.preventDefault();
+	
 	const data = {
 		name: document.getElementById('name').value,
 		age: document.getElementById('age').value,
@@ -16,34 +18,52 @@ $signInForm.addEventListener('submit', async (e) =>{
 		password: document.getElementById('pass').value
 	}
 
-	if (samePass()){
+	if (samePass() == false){
+		document.getElementById("error").style.display = "block";
+		error.innerHTML += `<li>Passwords must be the same!</li>`;
+		
+	}
+	if(validateAge() == false){
+		document.getElementById("error").style.display = "block";
+		error.innerHTML += `<li>Age must be valid whole number!</li>`;
+	}
+	else{
 		try{
 			const rawRes = await fetch('/users',{
 				method: 'post',
 				headers: {
 					'Accept': 'application/json',
 					"Content-Type": "application/json; charset=utf-8"
-				  },
+					},
 				body: JSON.stringify(data)
 			})
 			const res = await rawRes.json();
-			for (var key in res.errors){
-				var value = res.errors[key]
-				error.innerHTML += '<ul><li>' + value.message +'</li>';
-				console.log(value);
+			if (res.errors){
+				for (var key in res.errors){
+					var value = res.errors[key]
+					document.getElementById("error").style.display = "block";
+	
+					error.innerHTML += '<li>' + value.message +'</li>';
+					console.log(value);
+					console.log(res);
+					return;
+				}
+			}else{
+				error.innerHTML = '<li>Account successfully created</li>';
+				document.getElementById("error").style.background = "green";
+				document.getElementById("error").style.display = "block";
+
 			}
 			error.innerHTML += '</ul>';
-			console.log(res);
+			//window.location = "intent://example.com#Intent;schemehttp;/path"
+	
 		}
 		catch(error){
 			error.innerHTML = 'Something went wrong. . .';
 			console.log(error);
 		}
-		
 	}
-	else{
-		console.log('need to match passwords')
-	}
+
 });
 
 
@@ -56,4 +76,14 @@ const samePass = () => {
 		return false;
 	}
 	return true
+}
+
+
+const validateAge = () =>{
+	
+	//returns true for not a number so we are saying if it is a number ensure its an int
+	if(!isNaN(document.getElementById('age').value)){
+		return true;
+	}
+	return false;
 }
